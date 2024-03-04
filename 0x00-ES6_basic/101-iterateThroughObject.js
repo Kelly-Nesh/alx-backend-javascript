500dc892a31dfad0b590dbd1f50bad1281946181
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-05 02:05:56 +0300

    array.join

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+ export default function iterateThroughObject(reportWithIterator) {
+    const all = []
+    for (let i of reportWithIterator) {
+        all.push(i)
+    }
+    return all.join(" | ")
+ }
