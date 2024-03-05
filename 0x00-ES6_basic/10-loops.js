93f2bb0d75cd6b8c2fa2af7e6458bb2d29959bc8
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-05 03:23:22 +0300

    disabled no-restricted-syntax n no-param-reassign

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
+  // eslint-disable-next-line no-restricted-syntax
   for (const itm of array) {
     const value = itm;
+    // eslint-disable-next-line no-param-reassign
     array[array.indexOf(itm)] = appendString + value;
   }
 
