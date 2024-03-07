c77d6953b47e30f9303655015142df0158f049c4
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-07 22:20:56 +0300

    currency code name

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,24 @@
+/* eslint no-underscore-dangle: 0 */
+/* eslint-disable-next-line no-unused-vars */
+class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get name() { return this._name; }
+
+  set name(n) {
+    this._name = n;
+  }
+
+  get code() { return this._code; }
+
+  set code(c) {
+    this._code = c;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
