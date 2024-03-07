a1960e9817d834d6ce867b31ea8110bd30c621aa
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-07 12:35:00 +0300

    added return values

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  createUser()
-    .then((user) => {
-      uploadPhoto().then((photo) => {
-        console.log(photo.body, user.firstName, user.lastName);
-      }).catch(() => {
-        console.log('Signup system offline');
-      });
-    })
+  return createUser()
+    .then((user) => uploadPhoto().then((photo) => {
+      console.log(photo.body, user.firstName, user.lastName);
+      return { user, photo };
+    }))
     .catch(() => {
       console.log('Signup system offline');
     });
