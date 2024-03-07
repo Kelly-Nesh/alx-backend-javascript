5f06e6a9462d577f5cc8e4de8472939d22611d22
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-07 12:47:43 +0300

    eslint fixed

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.race([chinaDownload, USDownload]).then(first => {
-    return first;
-  });
+  return Promise.race([chinaDownload, USDownload]).then((first) => first);
 }
