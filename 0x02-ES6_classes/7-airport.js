aa558e4d2823f0e45332b74f08b88bb1a6106382
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-08 00:08:56 +0300

    symbol tostringtag

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,28 @@
+/* eslint no-underscore-dangle: 0 */
+/* eslint-disable-next-line no-unused-vars */
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(n) {
+    this._name = n;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(c) {
+    this._code = c;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this.code;
+  }
+}
