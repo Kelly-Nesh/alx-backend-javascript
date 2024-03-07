c40c495ff99e78cbca0276a43b58e607c656e3e3
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-08 01:50:38 +0300

    new superconstructor and styling eslint fixed

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,17 +1,21 @@
-import Car from "./10-car";
+/* eslint no-underscore-dangle: 0 */
+import Car from './10-car';
 
-export default class EVCar extends Car{
-    constructor(brand, motor, color, range) {
-        super(brand, motor, color)
-        this.range = range;
-    }
-    get range() {
-        return this._range;
-    }
-    set range(r) {
-        this._range = r
-    }
-    cloneCar() {
-        return new Car()
-    }
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(r) {
+    this._range = r;
+  }
+
+  cloneCar() {
+    return new super.constructor();
+  }
 }
