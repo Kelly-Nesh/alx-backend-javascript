62916858cb419d77c811ef0d5170219ccee85913
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-07 13:12:46 +0300

    return value changed to use templates

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return createUser()
     .then((user) => uploadPhoto().then((photo) => {
-      console.log(photo.body, user.firstName, user.lastName);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
       return { user, photo };
     }))
     .catch(() => {
