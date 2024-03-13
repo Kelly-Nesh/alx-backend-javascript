08f51fa76cf9cfd8198cd6a1804cac9f0b38c970
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-13 22:44:04 +0300

    added stringstart present and typecheck

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,12 @@
 export default function cleanSet(set, startString) {
   let string = '';
-  const len = startString.length;
-  for (const i of set) {
-    if (startString && i.startsWith(startString)) {
-      string += string ? '-' : '';
-      string += i.slice(len);
+  if (startString && typeof startString === 'string') {
+    const len = startString.length;
+    for (const i of set) {
+      if (i.startsWith(startString)) {
+        string += string ? '-' : '';
+        string += i.slice(len);
+      }
     }
   }
   return string;
