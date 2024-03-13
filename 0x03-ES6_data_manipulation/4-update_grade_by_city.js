a7abdcfb1d7805f9486c9774a9877339da26b273
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-13 21:59:20 +0300

    added grade const for assignment

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,7 @@
 export default function updateStudentGradeByCity(list, city, newGrades) {
   return list.map((e) => {
-    e.grade = newGrades.find((n) => n.studentId === e.id)?.grade || 'N/A';
+    const grade = newGrades.find((n) => n.studentId === e.id);
+    e.grade = grade ? grade.grade : 'N/A';
     return e;
   }).filter((e) => e.location === city);
 }
