c84a661cac986c767078ae038e5d9fc127d9d06c
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-14 14:48:14 +0300

    eslint format fix

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,9 @@
 export default function groceriesList() {
-    const map =  new Map([]);
-    [['Apples', 10],
+  const map = new Map([]);
+  [['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
-    ['Banana', 5]].forEach(e => map.set(e[0], e[1]))
-    return map
+    ['Banana', 5]].forEach((e) => map.set(e[0], e[1]));
+  return map;
 }
