07d87c73b18fc74f2887c89c2476319877a7e363
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-16 09:45:45 +0300

    class implements interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,28 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
     console.log(`${firstName[0]}. ${lastName}`)
 }
+
+interface Student {
+    firstName: string
+    lastName: string
+    workOnHomework(): string
+    displayName(): string
+}
+
+class StudentClass implements Student {
+    firstName: string
+    lastName: string
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+
+    workOnHomework(): string {
+        return 'Currently working'
+    }
+
+    displayName(): string {
+        return this.firstName
+    }
+}
